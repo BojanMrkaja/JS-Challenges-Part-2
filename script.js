2a31bd4ce09da1e176d8844eadad79183e40212c
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-08-28 17:40:17 +0200

    Which Generation Are You? challnege done

--- script.js
+++ script.js
@@ -32,3 +32,51 @@ const countTrue = function (arrOfBool) {
   }
 };
 // console.log(countTrue([true, false, false, true, false]));
+
+/*
+Which Generation Are You?
+
+Try finding your ancestors and offspring with code.
+
+Create a function that takes a number x and a character y ("m" for male, "f" for female), and returns the name of an ancestor (m/f) or descendant (m/f).
+
+    If the number is negative, return the related ancestor.
+    If positive, return the related descendant.
+    You are generation 0. In the case of 0 (male or female), return "me!".
+
+Examples
+
+generation(2, "f") ➞ "granddaughter"
+
+generation(-3, "m") ➞ "great grandfather"
+
+generation(1, "f") ➞ "daughter"
+
+Notes
+
+Check the Resources tab for helpful hints.
+Generation	Male	Female
+-3	great grandfather	great grandmother
+-2	grandfather	grandmother
+-1	father	mother
+0	me!	me!
+1	son	daughter
+2	grandson	granddaughter
+3	great grandson	great granddaughter
+*/
+
+const generation = function (gennaration, gender) {
+  const genObj = {
+    0: 'me',
+    1: gender === 'm' ? 'son' : 'daughter',
+    2: gender === 'm' ? 'grandson' : 'granddaughter',
+    3: gender === 'm' ? 'great grandson' : 'great granddaughter',
+    '-1': gender === 'm' ? 'father' : 'mother',
+    '-2': gender === 'm' ? 'grandfather' : 'grandmother',
+    '-3': gender === 'm' ? 'great grandfather' : 'great grandmother',
+  };
+
+  return genObj[gennaration];
+};
+
+console.log(generation(3, 'f'));
