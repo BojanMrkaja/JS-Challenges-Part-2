41053f494e617027c2a0cbdde520c4517c2eb33d
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-08-30 18:32:40 +0200

    25-Mile Marathon challenge done

--- script.js
+++ script.js
@@ -440,4 +440,73 @@ const findNemo = function (str) {
   }
 };
 
-console.log(findNemo('I am finding  !'));
+// console.log(findNemo('I am finding  !'));
+
+/*
+
+Find the Discount
+
+Create a function that takes two arguments: the original price and the discount percentage as integers and returns the final price after the discount.
+
+Alternative Text
+Examples
+
+dis(1500, 50) ➞ 750
+
+dis(89, 20) ➞ 71.2
+
+dis(100, 75) ➞ 25
+
+Notes
+
+Your answer should be rounded to two decimal places.
+
+*/
+
+const discount = function (price, discount) {
+  const discountToDecimal = discount / 100;
+  // console.log(discountToDecimal);
+  const priceWithDiscont = price * discountToDecimal;
+
+  // console.log(priceWithDiscont);
+};
+
+// console.log(discount(100, 75));
+
+/*
+
+25-Mile Marathon
+
+Mary wants to run a 25-mile marathon. When she attempts to sign up for the marathon, she notices the sign-up sheet doesn't directly state the marathon's length. Instead, the marathon's length is listed in small, different portions. Help Mary find out how long the marathon actually is.
+
+Return true if the marathon is 25 miles long, otherwise, return false.
+Examples
+
+marathonDistance([1, 2, 3, 4]) ➞ false
+
+marathonDistance([1, 9, 5, 8, 2]) ➞ true
+
+marathonDistance([-6, 15, 4]) ➞ true
+
+Notes
+
+    Items in the array will always be integers.
+    Items in the array may be negative or positive, but since negative distance isn't possible, find a way to convert negative integers into positive integers.
+    Return false if the arguments are empty or not provided.
+
+*/
+
+const marathonDistance = function (arr) {
+  let sumOfArr = 0;
+  const noNegativs = arr
+    .map(el => Math.abs(el))
+    .forEach(element => (sumOfArr += element));
+
+  if (sumOfArr === 25) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(marathonDistance([-6, 15, 4]));
