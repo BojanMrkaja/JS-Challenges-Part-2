84c3ae96ca6df85833d18526dabd9c13d137a54d
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-08-30 19:21:47 +0200

    Mirror Array challenge done

--- script.js
+++ script.js
@@ -531,8 +531,6 @@ Notes
 
     You will be given at least one box.
     Each box will always have three dimensions included.
-
-
 */
 
 function totalVolume(...boxes) {
@@ -548,4 +546,33 @@ function totalVolume(...boxes) {
   return totalVolume;
 }
 
-console.log(totalVolume([4, 2, 4], [3, 3, 3], [1, 1, 2], [2, 1, 1]));
+// console.log(totalVolume([4, 2, 4], [3, 3, 3], [1, 1, 2], [2, 1, 1]));
+
+/*
+
+Mirror Array
+
+Given an integer array, transform that array into a mirror.
+Examples
+
+mirror([0, 2, 4, 6]) ➞ [0, 2, 4, 6, 4, 2, 0]
+
+mirror([1, 2, 3, 4, 5]) ➞ [1, 2, 3, 4, 5, 4, 3, 2, 1]
+
+mirror([3, 5, 6, 7, 8]) ➞ [3, 5, 6, 7, 8, 7, 6, 5, 3]
+
+Notes
+
+Do not repeat the last item of the given array.
+
+*/
+
+const mirror = function (arr) {
+  const copyArr = [...arr];
+  const revArr = arr.reverse();
+  revArr.shift();
+  copyArr.push(...revArr);
+
+  return copyArr;
+};
+console.log(mirror([0, 2, 4, 6]));
