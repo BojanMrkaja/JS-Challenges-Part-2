7420b3b1130edf8c664369da1906c7155ed5f14c
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-08-30 19:32:20 +0200

    Let's Sort This Array!

--- script.js
+++ script.js
@@ -575,4 +575,35 @@ const mirror = function (arr) {
 
   return copyArr;
 };
-console.log(mirror([0, 2, 4, 6]));
+// console.log(mirror([0, 2, 4, 6]));
+
+/*
+
+Let's Sort This Array!
+
+Create a function that takes an array of numbers arr, a string str and return an array of numbers as per the following rules:
+
+    "Asc" returns a sorted array in ascending order.
+    "Des" returns a sorted array in descending order.
+    "None" returns an array without any modification.
+
+Examples
+
+ascDesNone([4, 3, 2, 1], "Asc" ) ➞ [1, 2, 3, 4]
+
+ascDesNone([7, 8, 11, 66], "Des") ➞ [66, 11, 8, 7]
+
+ascDesNone([1, 2, 3, 4], "None") ➞ [1, 2, 3, 4]
+*/
+
+const ascDesNone = function (arr, order) {
+  if (order === 'Asc') {
+    return arr.sort((a, b) => a - b);
+  } else if (order === 'Des') {
+    return arr.sort((a, b) => b - a);
+  } else if (order === 'None') {
+    return arr;
+  }
+};
+
+console.log(ascDesNone([7, 8, 11, 66], 'Des'));
