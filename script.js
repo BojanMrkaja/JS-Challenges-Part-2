31675e2a05e4b8e3fe42bc778057f1cf82d0b307
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-08-31 14:28:37 +0200

    Find NaN in an Array

--- script.js
+++ script.js
@@ -641,4 +641,31 @@ const matchLastItem = function (arr) {
   }
 };
 
-console.log(matchLastItem([8, 'thunder', true, '8thundertrue']));
+// console.log(matchLastItem([8, 'thunder', true, '8thundertrue']));
+
+/*
+
+Find NaN in an Array
+
+Create a function to find NaN in an array of numbers. The return value should be the index where NaN is found. If NaN is not found in the array, then return -1.
+Examples
+
+findNaN([1, 2, NaN]) ➞ 2
+
+findNaN([NaN, 1, 2, 3, 4]) ➞ 0
+
+findNaN([0, 1, 2, 3, 4]) ➞ -1
+
+Notes
+
+NaN will occur in the input array only once.
+
+*/
+
+const findNaN = function (arr) {
+  let newArr = [];
+  arr.forEach(el => newArr.push(String(el)));
+  return newArr.indexOf('NaN');
+};
+
+console.log(findNaN([0, 1, 2, 3, 4]));
